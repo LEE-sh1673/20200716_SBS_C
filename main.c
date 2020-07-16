a342001b9fa0c8c2f50b20549af00d7757868941
LEEsh1673 <lsh901673@gmail.com>
2020-07-16 12:05:17 +0000

    String array, operators

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#define STR_MAX 21
 
-int main(void) {
-  printf("Hello World");
+int main(void) 
+{
+  // incompleted type (initialization with string)
+  char name[STR_MAX] = {0, };
+ 
+  // declare with size and initialization with string.
+  char addr[STR_MAX] = "test@naver.com";
+
+  int data = 5;
+  int result = 0;
+
+  // input the string
+  printf("Enter your name (under the %d): ", STR_MAX);
+  scanf("%[^\n]s", name);
+
+  // print the string
+  printf("Name: %s\n", name);
+  printf("Addr: %s\n", addr);
+
+  // operators practice
+  result = data % 2;
+  printf("Op output: %d, %d\n", data, result); // 5, 1
+
+  result = data << 5;
+  printf("Op output: %d, %d\n", data, result); // 5, 150
+
+  result = (data << 4) + 3;
+  printf("Op output: %d, %d\n", data, result); // 5, 83
+
+  result = data == 5;
+  printf("Op output: %d, %d\n", data, result); // 5, 1
+
+  result = data != 5 && (data = 0);
+  printf("Op output: %d, %d\n", data, result); // 5, 0
+
+  result = --result && (data = 0);
+  printf("Op output: %d, %d\n", data, result); // 0, 0
+
+  result = result-- && (data = 0);
+  printf("Op output: %d, %d\n", data, result); // 0, 0
+
+  result = result-- || (data = 0);
+  printf("Op output: %d, %d\n", data, result); // 0 ,0
+
   return 0;
 }
